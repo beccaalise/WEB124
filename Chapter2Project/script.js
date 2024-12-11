85453035bc363ce954b6773cb8ca184166b4a6a8
beccaalise <beccaacathey@gmail.com>
2024-12-11 16:28:41 -0600

    Create script.js

---
+++ Chapter2Project/script.js
@@ -0,0 +1,35 @@
+//author Rebecca Cathey
+let myName = "Rebecca Cathey";
+
+let para1 = document.getElementById("p1");
+para1.textContent = myName;
+
+let n1 = 5;
+let n2 = 10;
+
+let numberSum = n1 + n2;
+document.getElementById("p2").textContent = numberSum;
+
+let numberMult = n1 * n2;
+document.getElementById("p3").textContent = numberMult;
+
+let myNameAddNum = myName + n1;
+document.getElementById("p4").textContent = myNameAddNum;
+
+let myNameMultNum = myName * n1;
+document.getElementById("p5").textContent = myNameMultNum;
+
+let age = 23;
+let ageCompare = age > numberMult;
+document.getElementById("p6").textContent = ageCompare;
+
+n1 = 8;
+n2 = 12;
+
+let updatedNumberSum = n1 + n2;
+let updatedNumberMult = n1 * n2;
+let updatedAgeCompare = age > updatedNumberMult;
+
+document.getElementById("p2").textContent = updatedNumberSum;
+document.getElementById("p3").textContent = updatedNumberMult;
+document.getElementById("p6").textContent = updatedAgeCompare;
